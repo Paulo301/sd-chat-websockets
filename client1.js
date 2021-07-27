4b2829e476afbc1823e6560e949e79866edd3fe5
Paulo301 <paulovictor1918@gmail.com>
2021-07-27 20:26:50 -0300

    final commit

--- client1.js
+++ client1.js
@@ -19,10 +19,14 @@ socket.on("message", (data) => {
   if(obj.command === 'create_user'){
     user = obj.user;
   }else if(obj.command === 'joined_room'){
-    room = obj.data;
+    room = obj.data.room;
     inRoom = true;
 
     console.log(`Você está na sala: ${room}`);
+    console.log("Usuários na sala:");
+    obj.data.participants.map((p) => {
+      console.log(`>>${p}`);
+    });
     console.log("Se você for administrador pode digitar '/ban nome_do_usuario' para banir um usuário da sala");
     console.log("Você pode a qualquer momento digitar '/quit' para sair da sala");
   }else if(obj.command === 'message'){
@@ -31,16 +35,54 @@ socket.on("message", (data) => {
     console.log("Salas")
     obj.data.map((room) => {
       console.log(`>>>${room.room}`);
-    })
+    });
   }else if(obj.command === 'failed_to_join'){
     console.log(obj.data);
+  }else if(obj.command === 'quit_success'){
+    inRoom = false;
+
+    console.log("Você deixou a sala com sucesso");
+
+    console.log("Digite:"+
+                "\n>>>'/join nome_da_sala' -> para entrar numa das salas existentes"+
+                "\n>>>'/create nome_da_sala' -> para criar uma sala nova se for administrador"+
+                "\n>>>'/list' -> para listar as salas");
+
+    console.log("Salas");
+    obj.data.map((room) => {
+      console.log(`>>>${room.room}`);
+    });
+  }else if(obj.command === 'create_success'){
+    room = obj.data;
+    inRoom = true;
+    console.log("Sala criada com sucesso!");
+    console.log("Você está agora na sala que criou, digite /quit para sair");
+  }else if(obj.command === 'ban_success'){
+    console.log(obj.data);
+  }else if(obj.command === 'failed_ban'){
+    console.log(obj.data);
+  }else if(obj.command === 'create_fail'){
+    console.log(obj.data);
+  }else if(obj.command === 'banned'){
+    room = "";
+    inRoom = false;
+    console.log(`Você foi banido da sala ${obj.data.room} pelo usuário ${obj.user}` );
+    console.log("Digite:"+
+                "\n>>>'/join nome_da_sala' -> para entrar numa das salas existentes"+
+                "\n>>>'/create nome_da_sala' -> para criar uma sala nova se for administrador"+
+                "\n>>>'/list' -> para listar as salas");
+
+    console.log("Salas");
+    obj.data.rooms.map((room) => {
+      console.log(`>>>${room.room}`);
+    });
   }
 });
 
 console.log("Digite:"+
                 "\n>>>'/join nome_da_sala' -> para entrar numa das salas existentes"+
                 "\n>>>'/create nome_da_sala' -> para criar uma sala nova se for administrador"+
-                "\n>>>'/delete nome_da_sala' -> para deletar uma sala nova se for administrador");
+                "\n>>>'/list' -> para listar as salas");
 
 rl.on('line', line => {
   // if (line === "FIM") {
@@ -48,8 +90,19 @@ rl.on('line', line => {
   // }
   if(line.slice(0,5)==="/join" && (inRoom === false)){ 
     socket.send(JSON.stringify({user: user, command: 'join', data: line.slice(6)}));
+  } else if(line.slice(0,7)==="/create" && inRoom===false){
+    socket.send(JSON.stringify({user: user, command: 'create', data: line.slice(8)}));
+  }else if(line.slice(0,5)==="/list" && inRoom===false){
+    socket.send(JSON.stringify({user: user, command: 'list', data: ""}));
+  }else if(line.slice(0,5)==="/quit" && inRoom===true){
+    socket.send(JSON.stringify({user: user, command: 'quit', data: room}));
+  }else if(line.slice(0,4)==="/ban" && inRoom===true){
+    console.log("entrou");
+    socket.send(JSON.stringify({user: user, command: 'ban', data: {user: line.slice(5), room: room}}));
   } else if(inRoom===true){
     socket.send(JSON.stringify({user: user, command: 'message', data: {room, message: line}}));
+  } else {
+    console.log("Você digitou um comando inválido!");
   }
 
 });
--- index.js
+++ index.js
@@ -22,26 +22,95 @@ wss.on('connection', function connection(ws) {
             return room;
           }
         });
-        ws.send(JSON.stringify({user: obj.user, command: "joined_room", data: tempRoom.room}));
+        ws.send(JSON.stringify({user: obj.user, command: "joined_room", data: {room: tempRoom.room, participants: tempRoom.participants}}));
       } else{
         ws.send(JSON.stringify({user: obj.user, command: "failed_to_join", 
                                 data: "Ocorreu uma falha para entrar na sala"}));
       }
 
       console.log(rooms);
+    }else if(obj.command === 'list'){
+      const userIndex = users.findIndex((user) => user.user === obj.user);
+
+      if(userIndex !== -1){
+        usersWS[userIndex].send(JSON.stringify({user: obj.user, command: 'list_rooms', data: rooms}));
+      }
     }else if(obj.command === 'message'){
       const tempRoom = rooms.find((room) => room.room === obj.data.room);
       const indexes = users.map((user, index) => {
         if(tempRoom.participants.includes(user.user) && (user.user !== obj.user)){
-          console.log("Aqui");
           return index;
         }
       });
       indexes.map((i) => {
         if(i!==undefined){
-          usersWS[i].send(JSON.stringify({user: obj.user, command: "message", data: obj.data.message}))
+          usersWS[i].send(JSON.stringify({user: obj.user, command: "message", data: obj.data.message}));
         }
       });
+    } else if(obj.command === 'create'){
+      const userIndex = users.findIndex((user) => user.user === obj.user);
+
+      if(userIndex !== -1){
+        if(users[userIndex].level === "admnistrator"){
+          rooms.push({room: obj.data, participants: [obj.user]});
+          usersWS[userIndex].send(JSON.stringify({user: obj.user, command: "create_success", data: obj.data}));
+        } else{
+          usersWS[userIndex].send(JSON.stringify({user: obj.user, command: "create_fail", data: "Você não tem permissão para criar salas"}));
+        }
+      }
+      console.log(rooms);
+    } else if(obj.command === 'quit'){
+      const userIndex = users.findIndex((user) => user.user === obj.user);
+      const tempRoom = rooms.find((room) => room.room === obj.data);
+
+      if(tempRoom !== undefined){
+        tempRoom.participants = tempRoom.participants.filter((p) => p !== obj.user);
+        rooms = rooms.map((room) => {
+          if(room.room === tempRoom.room){
+            return tempRoom;
+          }else{
+            return room;
+          }
+        });
+      }
+
+      if(userIndex !== -1){
+        usersWS[userIndex].send(JSON.stringify({user: obj.user, command: "quit_success", data: rooms}));
+      }
+
+      console.log(rooms);
+    }
+    else if(obj.command === 'ban'){
+      const userIndex = users.findIndex((user) => user.user === obj.user);
+      const user = users.find((user) => user.user === obj.user);
+
+      if(user !== undefined){
+        if(user.level === "admnistrator"){
+          const userToBanIndex = users.findIndex((user) => user.user === obj.data.user);
+          const tempRoom = rooms.find((room) => room.room === obj.data.room);
+
+          if(tempRoom !== undefined){
+            tempRoom.participants = tempRoom.participants.filter((p) => p !== obj.data.user);
+            rooms = rooms.map((room) => {
+              if(room.room === tempRoom.room){
+                return tempRoom;
+              }else{
+                return room;
+              }
+            });
+          }
+
+          if(userToBanIndex !== -1){
+            usersWS[userIndex].send(JSON.stringify({user: obj.user, command: "ban_success", data: "Usuário banido com sucesso"}));
+            usersWS[userToBanIndex].send(JSON.stringify({user: obj.user, command: "banned", data: {room: obj.data.room, rooms: rooms}}));
+          } else{
+            usersWS[userIndex].send(JSON.stringify({user: obj.user, command: "failed_ban", data: "Usuário não encontrado"}));
+          }
+        } else{
+          usersWS[userIndex].send(JSON.stringify({user: obj.user, command: "failed_ban", data: "Você não tem permissão para banir."}));
+        }
+      }
+      console.log(rooms);
     }
   });
 
